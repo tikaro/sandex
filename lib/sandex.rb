b8dd689dae1a8baa2e98525b15d0e7996d3a2826
tikaro <john@tikaro.net>
2009-03-09 22:16:01 -0400

    let's see if this works as a gemtest.radioize ; '

--- lib/sandex.rb
+++ lib/sandex.rb
@@ -1,5 +1,25 @@
+require 'rubygems'
+gem 'libxml-ruby', '>= 0.9.7'
+gem 'andre-geokit', '>= 1.2.0'
+gem 'outoftime-noaa', '>= 0.2.2'
+
+require 'time'
+require 'libxml'
+require 'geokit'
+require 'noaa'
+
 module Sandex
-  def self.calculate(station)
-    .98
+  def self.calculate(local_station='KILG')
+    local_conditions = NOAA.current_conditions_at_station(local_station)
+    ksan_conditions  = NOAA.current_conditions_at_station('KSAN')
+    
+    local_temperature = local_conditions.temperature
+    ksan_temperature  = ksan_conditions.temperature
+    
+    temperature_delta = ( ksan_temperature - local_temperature ).abs
+    
+    sandex_score = ( 100 - temperature_delta.to_f ) / 100
+    
+    sandex_score
   end
 end
