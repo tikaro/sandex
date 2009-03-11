d7ab049f9e34e9dfdb1011495feaf3c8ccbe6c86
tikaro <john@tikaro.net>
2009-03-11 16:43:26 -0400

    adding a fanger_box module to calculate if you're within parameters

--- lib/sandex.rb
+++ lib/sandex.rb
@@ -8,18 +8,79 @@ require 'libxml'
 require 'geokit'
 require 'noaa'
 
+%w(fanger_box).each { |file| require File.join(File.dirname(__FILE__), 'sandex', file) }
+
 module Sandex
-  def self.calculate(local_station='KILG')
-    local_conditions = NOAA.current_conditions_at_station(local_station)
-    ksan_conditions  = NOAA.current_conditions_at_station('KSAN')
     
-    local_temperature = local_conditions.temperature
-    ksan_temperature  = ksan_conditions.temperature
+    def self.calculate(station='KILG', verbose=nil)
+      
+      # KCOS: Colorado Springs
+      # KILG: West Chester
+      # KNYC: Central Park
+      
+      sandex_score          = 100
+      humidity_decrement    = 0
+      temperature_decrement = 0
+      msg = ""
+
+      humidity_penalty      = 2.5
+      heat_penalty          = 2
+      cold_penalty          = 1
+
+      current_conditions = NOAA.current_conditions_at_station(station)
+      t  = current_conditions.temperature
+      rh = current_conditions.relative_humidity
+      
+      msg += "At station #{station}, it's currently #{t} degrees and #{rh}% humidity.\n\n"
+
+      # Are we within the Fanger box?
+      if FangerBox.acceptable?(t,rh)
+        msg += "Conditions are WITHIN the Fanger Box!\n"
+      else
+        
+        msg += "Sadly, conditions are not inside the Fanger Box:\n"
+        
+        # calculate the humidity decrement
+        if FangerBox.too_dry?(rh)
+            msg += "* It's too dry.\n"
+        elsif FangerBox.too_humid?(rh)
+            humidity_delta = rh - FangerBox::HIGHEST_ALLOWABLE_HUMIDITY
+            humidity_decrement = humidity_delta * humidity_penalty
+            msg += "* It's too humid: (#{humidity_delta}% outside the Fanger Box)\n"
+            msg += "  Decrement: #{humidity_penalty} Sandex points for each percent of humidity.\n"
+            msg += "  Subtracting #{humidity_decrement} points from the Sandex.\n"
+        else
+            msg += "* The humidity is within the box, but the temperature is outside it.\n"
+        end
+
+        # calculate the temperature decrement
+        if FangerBox.too_cold?(t,rh)
+          cold_delta = FangerBox.minimum_temperature_at_humidity(rh) - t
+          temperature_decrement = cold_delta * cold_penalty
+          msg += "* It's #{cold_delta} degrees too chilly for the Fanger Box.\n"
+          msg += "  Decrement: #{cold_penalty} Sandex points for each degree too cold.\n"
+          msg += "  Subtracting #{temperature_decrement} points from the Sandex.\n"
+        elsif FangerBox.too_hot?(t,rh)
+          heat_delta = t - FangerBox.maximum_temperature_at_humidity(rh)
+          temperature_decrement = heat_delta * heat_penalty
+          msg += "* It's #{heat_delta} degrees too hot for the Fanger Box.\n"
+          msg += "  Decrement: #{heat_penalty} Sandex points for each degree too hot.\n"
+          msg += "  Subtracting #{temperature_decrement} points from the Sandex.\n"
+        else
+          msg += "* Temperature is within the Fanger box, though the humidity is outside it."
+        end
+      
+      end
+      
+      sandex_score -= humidity_decrement
+      sandex_score -= temperature_decrement
+
+      msg += "\nThe Sandex score for station #{station} is #{sandex_score}.\n"
     
-    temperature_delta = ( ksan_temperature - local_temperature ).abs
-    
-    sandex_score = ( 100 - temperature_delta.to_f ) / 100
-    
-    sandex_score
-  end
+      puts msg if verbose == "verbose"
+      
+      sandex_score
+      
+    end
+  
 end
---
+++ lib/sandex/fanger_box.rb
@@ -0,0 +1,50 @@
+module FangerBox
+  
+  LOWEST_ALLOWABLE_HUMIDITY  = 0
+  HIGHEST_ALLOWABLE_HUMIDITY = 60
+  
+  def self.acceptable?(t,rh)
+    # I tried writing this as A < B < C, but it didn't work.  Huh.
+    t > minimum_temperature_at_humidity(rh) && t < maximum_temperature_at_humidity(rh)
+  end
+  
+  def self.rh_acceptable?(rh)
+    # A < B < C works here.
+    LOWEST_ALLOWABLE_HUMIDITY < rh < HIGHEST_ALLOWABLE_HUMIDITY
+  end
+  
+  def self.too_dry?(rh)
+    # Fanger believed that humidity below 30% felt weird, but not uncomfortable.
+    rh <= LOWEST_ALLOWABLE_HUMIDITY
+  end
+  
+  def self.too_humid?(rh)
+    # Fanger believed that humidity above 60% was pretty much always crappy.
+    rh > HIGHEST_ALLOWABLE_HUMIDITY
+  end
+  
+  def self.too_cold?(t,rh)
+    t < minimum_temperature_at_humidity(rh)
+  end
+  
+  def self.too_hot?(t,rh)
+    t > maximum_temperature_at_humidity(rh)
+  end
+  
+  def self.minimum_temperature_at_humidity(rh)
+    # Based on the two bottom corners of the Fanger box:
+    # * 69 degrees at 30% relative humidity
+    # * 68 degrees at 60% relative humidity
+    ((1/30) * rh ) + 70
+  end
+  
+  def self.maximum_temperature_at_humidity(rh)
+    # Based on the top two corners of the Fanger box:
+    # * 82 degrees at 30% relative humidity
+    # * 78 degrees at 60% relative humidity
+    ((4/30) * rh ) + 86
+  end
+  
+
+
+end
