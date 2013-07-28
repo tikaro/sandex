f4fb4c3eb8bd7d7d4d2bf497c50480c90ad37e71
John Young <john@tikaro.net>
2013-07-27 23:52:24 -0400

    Now with an icon

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,11 @@
 class WelcomeController < ApplicationController
   def index
-    my_latitude = 39.9893
-    my_longitude = -75.6120
+    
+    @location = params[:loc]  ||= "West Chester, PA"
+    latitude = params[:lat]   ||= 39.9606
+    longitude = params[:lon]  ||= -75.6058
 
-    @forecast = ForecastIO.forecast(my_latitude, my_longitude, params: {exclude: "minutely,daily,alerts,flags"} )
+    @forecast = ForecastIO.forecast(latitude, longitude, params: {exclude: "minutely,daily,alerts,flags"} )
 
     @current_temp      = @forecast.currently.temperature.round
     @current_humidity  = (100*(@forecast.currently.humidity)).round
