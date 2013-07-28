e4faf03344196a8d23e42a3e28ea2005afefb417
John Young <john@tikaro.net>
2013-07-27 20:59:07 -0400

    new rails site

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Sandex::Application.config.session_store :cookie_store, key: '_sandex_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Sandex::Application.config.session_store :active_record_store
