a8d8463cda56161bd007e25092853fd1f09f681d
John Young <john.young@thinkcompany.com>
2023-06-03 19:02:54 -0400

    Remove create-react-app

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders the application', () => {
-  render(<App />);
-  const textElement = screen.getByText(/Sandex/i);
-  expect(textElement).toBeInTheDocument();
-});
--- src/js/temperatureLowerBound.mjs
+++
@@ -1,8 +0,0 @@
-export default function temperatureLowerBound(percentageHumidity) {
-    // at 30% relative humidity, anything below 69° is too cold.
-    // at 60% relative humidity, anything below 68° is too cold.
-    let temperatureLowerBound = (-3.3333333 * percentageHumidity ) + 70;
-    
-    // console.log('temperatureLowerBound is ' + temperatureLowerBound);
-    return temperatureLowerBound;
-}
