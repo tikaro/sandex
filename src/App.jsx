46dfdc438e8483547f607bfbf31cfca8fed94d86
John Young <john@tikaro.net>
2026-02-28 00:48:53 +0000

    Update to using Open-Meteo

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,53 @@
-import Forecast from './json/forecast.json';
-import HourRow from './components/HourRow.jsx';
+import Forecast from "./json/forecast-openmeteo.json";
+import HourRow from "./components/HourRow.jsx";
 
 const forecast = Forecast;
-const hours = forecast.data.timelines[0].intervals;
+const hours = forecast.hourly.time.map((startTime, index) => ({
+  startTime,
+  temperature: forecast.hourly.temperature_2m[index],
+  dewpoint: forecast.hourly.dew_point_2m[index],
+}));
 
 function App() {
-
-  const listOfHours = hours.map(hour =>
-    <HourRow 
-      key = {hour.startTime} 
-      startTime = {hour.startTime}
-      temperature = {hour.values.temperature}
-      dewpoint =  {hour.values.dewPoint}
+  const listOfHours = hours.map((hour) => (
+    <HourRow
+      key={hour.startTime}
+      startTime={hour.startTime}
+      temperature={hour.temperature}
+      dewpoint={hour.dewpoint}
     />
-  );
+  ));
 
   return (
     <div className="App">
       <>
-      <div id="header">
-        <h1>Sandex</h1>
-      </div>
-      <div id="forecast">
-      <table className="styled-table">
-          <thead>
-          <tr>
-            <th>Time</th>
-            <th><span title="Temperature">Temp</span></th>
-            <th><span title="Dewpoint">Dew</span></th>
-          </tr>
-          </thead>
-          <tbody>
-            { listOfHours }
-          </tbody>
-      </table>
-      </div>
-      <div id="wifir">
-        <img src="/wifir.png" alt="The wifir is the logo for 'coworkout', an outdoor co-working group" width="249px" height="249px" />
-      </div>
+        <div id="header">
+          <h1>Sandex</h1>
+        </div>
+        <div id="forecast">
+          <table className="styled-table">
+            <thead>
+              <tr>
+                <th>Time</th>
+                <th>
+                  <span title="Temperature">Temp</span>
+                </th>
+                <th>
+                  <span title="Dewpoint">Dew</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody>{listOfHours}</tbody>
+          </table>
+        </div>
+        <div id="wifir">
+          <img
+            src="/wifir.png"
+            alt="The wifir is the logo for 'coworkout', an outdoor co-working group"
+            width="249px"
+            height="249px"
+          />
+        </div>
       </>
     </div>
   );
